8a153f4a62d9f699a3dab44296bfd3fbb3cd2ef5
Camila Moura <crmcelestino@gmail.com>
2022-04-08 14:48:05 -0300

    Create Entity Professor

---
+++ src/main/java/com/project/professorallocation/model/Professor.java
@@ -0,0 +1,57 @@
+package com.project.professorallocation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Professor {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@Column (length = 11, nullable = false, unique = true)
+	private String cpf;
+	
+	private Long departmentId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	
+	
+}
